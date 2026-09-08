3185dbd652b24f466e22f9d29cdced6e071cf8de
CyrillGerardCajucom <cyrillgerardc@gmail.com>
2026-09-09 02:51:56 +0800

    downgraded to base.en Whisper speech-to-text model

--- ascend-vision/config.py
+++ ascend-vision/config.py
@@ -332,7 +332,7 @@ class DashboardConfig:
 @dataclass(frozen=True)
 class VoiceCommandConfig:
     enabled: bool = True
-    model_size: str = 'small.en'
+    model_size: str = 'base.en'
     compute_type: str = 'int8'
     device: str = 'cpu'
     device_index: int | None = None
